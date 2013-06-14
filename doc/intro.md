5c47de16195ec78e5649116f11e2c3a7a9c7fdab
Jani Arvonen <jani.arvonen@gmail.com>
2013-06-14 11:21:28 +0300

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to whereabts-notify
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/whereabts_notify/core_test.clj
@@ -0,0 +1,7 @@
+(ns whereabts-notify.core-test
+  (:use clojure.test
+        whereabts-notify.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
