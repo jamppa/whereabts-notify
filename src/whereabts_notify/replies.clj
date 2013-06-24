84c23bcc6b910a471af905ae786687063b3a39b5
Jani Arvonen <jani.arvonen@gmail.com>
2013-06-24 19:34:47 +0300

    added reply-nick and reply-message to gcm-message when notifiying user

--- src/whereabts_notify/replies.clj
+++ src/whereabts_notify/replies.clj
@@ -1,18 +1,22 @@
 (ns whereabts-notify.replies
 	(:use 
 		[whereabts-notify.gcm]
-		[whereabts-notify.db.users]))
+		[whereabts-notify.db.users]
+		[whereabts-notify.db.replies]))
 
 (def reply-message-type "TYPE_MESSAGE_REPLY")
 
-(defn- replies-gcm-message [gcm-id details]
+(defn- replies-gcm-message [gcm-id reply message-id]
 	(gcm-message [gcm-id] 
 		{
 			:type reply-message-type 
-			:message-id (:replied-message details)
-			:reply (:reply details)}))
+			:message-id message-id
+			:reply-nick (:nick reply)
+			:reply-message (:replymessage reply)}))
 
 (defn notify-user-on-reply [details]
 	(if-let [user-gcm-id (find-user-gcm-id (:user-to-notify details))]
-		(send-gcm-message (replies-gcm-message user-gcm-id details))
-		nil))
+		(if-let [reply (find-reply (:reply details))]
+			(send-gcm-message 
+				(replies-gcm-message user-gcm-id reply (:replied-message details)))
+		nil)))
--- test/whereabts_notify/replies_test.clj
+++ test/whereabts_notify/replies_test.clj
@@ -3,19 +3,23 @@
 		[midje.sweet]
 		[whereabts-notify.replies]
 		[whereabts-notify.db.users]
+		[whereabts-notify.db.replies]
 		[whereabts-notify.gcm]))
 
-(def reply-details {:replied-message "abc" :user-to-notify "123" :reply {:key "val"}})
+(def reply {:nick "seppo" :replymessage "cool!"})
+(def reply-details {:replied-message "abc" :user-to-notify "123" :reply "567"})
 (def reply-gcm-message {
 		:registration_ids ["abc"] 
 		:data {
 			:type "TYPE_MESSAGE_REPLY" 
 			:message-id (:replied-message reply-details)
-			:reply (:reply reply-details)}})
+			:reply-nick "seppo"
+			:reply-message "cool!"}})
 
 (fact "should notify user on reply by sending gcm message"
 	(notify-user-on-reply reply-details) => anything
 	(provided (find-user-gcm-id "123") => "abc" :times 1)
+	(provided (find-reply "567") => reply :times 1)
 	(provided (send-gcm-message reply-gcm-message) => anything :times 1))
 
 (fact "should not notify user on reply by sending gcm message when gcm id is not found for the user"
