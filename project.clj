8872da2fdf070bdbb21631d4d1d57a54bfbf8bbf
Jani Arvonen <jani.arvonen@gmail.com>
2013-07-24 10:22:41 +0300

    updated carmine to 2.0.0

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
         [[org.clojure/clojure "1.5.1"]
         [com.novemberain/monger "1.5.0"]
         [com.novemberain/validateur "1.4.0"]
-				[com.taoensso/carmine "1.12.0"]
+				[com.taoensso/carmine "2.0.0"]
         [clj-http "0.7.0"]
         [org.clojure/data.json "0.2.2"]]
    :profiles {
