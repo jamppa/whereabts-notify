32e83bcece3b5ba2c61c328cfb8256102da31c2d
Jani Arvonen <jani.arvonen@gmail.com>
2013-08-08 15:04:43 +0300

    updated monger and validateur libs

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies 
         [[org.clojure/clojure "1.5.1"]
-        [com.novemberain/monger "1.5.0"]
-        [com.novemberain/validateur "1.4.0"]
+        [com.novemberain/monger "1.6.0"]
+        [com.novemberain/validateur "1.5.0"]
 				[com.taoensso/carmine "2.0.0"]
         [clj-http "0.7.0"]
         [org.clojure/data.json "0.2.2"]]
