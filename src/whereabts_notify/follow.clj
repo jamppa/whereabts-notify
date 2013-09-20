5cff5bf44c13e233deee3cbfc84a52169c2918a4
Jani Arvonen <jani.arvonen@gmail.com>
2013-09-20 15:22:26 +0300

    should build follow gcm message from details

--- src/whereabts_notify/follow.clj
+++ src/whereabts_notify/follow.clj
@@ -3,8 +3,15 @@
 		whereabts-notify.gcm
 		whereabts-notify.db.users))
 
-(defn build-follow-gcm-message [follow-details]
+(def follow-message-type "TYPE_MESSAGE_NEW_FOLLOWER")
+
+(defn follow-gcm-data [follower-profile]
 	nil)
+
+(defn build-follow-gcm-message [{follower-id :follower-id following-id :following-id}]
+	(let [follower-profile (find-profile-by-user-id follower-id)
+		  following-gcm-id (find-user-gcm-id following-id)]
+		(gcm-message [following-gcm-id] (follow-gcm-data follower-profile))))
 
 (defn notify-user-on-new-follower [follow-details]
 	(-> 
--- test/whereabts_notify/follow_test.clj
+++ test/whereabts_notify/follow_test.clj
@@ -2,12 +2,30 @@
 	(:use
 		midje.sweet
 		whereabts-notify.follow
-		whereabts-notify.gcm))
+		whereabts-notify.gcm
+		whereabts-notify.db.users))
 
-(def follow-details {})
+(def follow-details {:follower-id "asdasd" :following-id "qweqwe"})
 
 (fact "should build gcm message and notify user on new follower"
 	(notify-user-on-new-follower follow-details) => anything
 	(provided
 		(build-follow-gcm-message follow-details) => anything :times 1
 		(send-gcm-message anything) => anything :times 1))
+
+(def built-follow-gcm-message {
+	:registration_ids ["QWEasd"]
+	:data {
+			:type follow-message-type
+			:follower-id "asdasd"
+			:follower-nick "testman"
+		}
+	})
+
+(fact "should build gcm message from details"
+	(build-follow-gcm-message follow-details) => anything
+	(provided
+		(find-profile-by-user-id "asdasd") => anything :times 1
+		(find-user-gcm-id "qweqwe") => anything :times 1
+		(follow-gcm-data anything) => anything :times 1
+		(gcm-message anything anything) => anything :times 1))
